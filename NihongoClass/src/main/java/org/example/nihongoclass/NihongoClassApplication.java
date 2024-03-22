6ea2a82556faea2580db2de2ba2650a36a0d8745
QuangThanh3001 <quangthanh33200@gmail.com>
2024-03-22 20:39:20 +0700

    trang index

---
+++ NihongoClass/src/main/java/org/example/nihongoclass/NihongoClassApplication.java
@@ -0,0 +1,13 @@
+package org.example.nihongoclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NihongoClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NihongoClassApplication.class, args);
+    }
+
+}
